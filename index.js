b6b0635cb772cc135134d2f247729c7499a1d867
masud <abdullahalmasud01737906772@gmail.com>
2022-05-25 17:37:00 +0600

    get and post porduct

--- index.js
+++ index.js
@@ -16,14 +16,49 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-        await client.connect();
-        console.log('database connected')
-    }
-    finally{
+async function run() {
+  try {
+    await client.connect();
+    const reviewCollection = client.db('af_electronic_ltd').collection('reviews');
+    const productCollection = client.db('af_electronic_ltd').collection('products');
 
-    }
+    // get review  api
+    app.get('/review', async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    })
+
+    // post review api
+    app.post('/review', async (req, res) => {
+      const newReview = req.body;
+      const result = await reviewCollection.insertOne(newReview);
+      res.send(result);
+    })
+
+    // get product api
+    app.get('/product', async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    })
+
+    // post product api
+    app.post('/product', async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+
+
+
+  }
+  finally {
+
+  }
 
 }
 
