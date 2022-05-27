d4b0f279a60c37d7b7627cdded3c50416c7023e6
masud <abdullahalmasud01737906772@gmail.com>
2022-05-27 10:09:03 +0600

    another commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 const port = process.env.PORT || 5000;
 
 
@@ -22,6 +22,16 @@ async function run() {
     const reviewCollection = client.db('af_electronic_ltd').collection('reviews');
     const productCollection = client.db('af_electronic_ltd').collection('products');
     const orderCollection = client.db('af_electronic_ltd').collection('order');
+    const userCollection = client.db('af_electronic_ltd').collection('users');
+
+    /*     // auth 
+        app.post('/login', async(req, res) =>{
+          const user = req.body;
+          const accessToken =jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: '1d'
+          });
+          res.send({ accessToken });
+        }) */
 
     // get review  api
     app.get('/review', async (req, res) => {
@@ -47,9 +57,9 @@ async function run() {
     });
 
     // get porduct purchase api
-    app.get('/product/:id', async(req, res) =>{
+    app.get('/product/:id', async (req, res) => {
       const id = req.params.id;
-      const query ={_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const product = await productCollection.findOne(query);
       res.send(product);
     })
@@ -60,13 +70,42 @@ async function run() {
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     })
-    
-    // order collection api
-    app.post('/order', async(req, res) =>{
+
+    // order post api
+    app.post('/order', async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
       res.send(result);
     })
+
+    // order get api
+    app.get('/order', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray()
+      res.send(orders);
+    });
+
+    app.put('/user/:email', async(req, res) =>{
+      const email = req.params.email;
+      const user = req.body;
+      const filter = {email: email};
+      const options = {upsert: true};
+      const updateDoc ={
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+      res.send({result, token});
+    })
+
+
+    // make admin user api
+/*     app.get('/user', async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    }) */
 
   }
   finally {
