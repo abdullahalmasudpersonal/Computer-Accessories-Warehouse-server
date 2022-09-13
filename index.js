1685bf88a37e7b407f9e8c24215ba370ffa756e3
masud <abdullahalmasud01737906772@gmail.com>
2022-09-14 00:55:54 +0600

    another big update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-/* function verifJWT(req, res, next) {
+/*  function verifJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
     return res.status(401).send({ message: 'UnAuthorized access' });
@@ -30,14 +30,39 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
     next();
   });
 } */
+function verifJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' });
+    }
+    console.log('decoded', decoded);
+    req.decoded = decoded;
+    next();
+  })
+}
 
 async function run() {
-  try{
+  try {
     await client.connect();
     const inventoriesCollection = client.db('Computer-Accessories-Warehouse').collection('inventories');
+    const orderCollection = client.db('Computer-Accessories-Warehouse').collection('order');
+
+    // Auth
+    app.post('/login', async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d'
+      });
+      res.send({ accessToken });
+    })
 
     // Inventories 
-    app.get('/inventories', async (req, res) =>{
+    app.get('/inventories', async (req, res) => {
       const query = {};
       const cursor = inventoriesCollection.find(query);
       const inventories = await cursor.toArray();
@@ -49,7 +74,72 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const inventories = await inventoriesCollection.findOne(query);
       res.send(inventories);
-  });
+    });
+
+    // order collection api
+    /*   app.get('/order', async (req, res) => {
+        const decodedEmail = req.decoded.email;
+        const email = req.query.email;
+        if (email === decodedEmail) {
+          const query = { email: email };
+          const cursor = orderCollection.find(query);
+          const orders = await cursor.toArray();
+          res.send(orders);
+        }
+        else{
+          res.status(403).send({message:'forbidden access'})
+        }
+      })  */
+
+    app.get('/order', verifJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      }
+      else {
+        res.status(403).send({ message: 'forbidden access' })
+      }
+    });
+
+    /*  app.get('/order', async (req, res) => {
+       const query = {};
+       const cursor = orderCollection.find(query);
+       const orders = await cursor.toArray();
+       res.send(orders);
+     }); */
+
+    app.post('/order', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+
+    // delete order api
+    app.delete('/order/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // add item api
+    app.post('/inventories', async (req, res) => {
+      const newInventories = req.body;
+      const result = await inventoriesCollection.insertOne(newInventories);
+      res.send(result);
+    });
+
+    // delete item api
+    app.delete('/inventories/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await inventoriesCollection.deleteOne(query);
+      res.send(result);
+    });
 
   }
   finally {
