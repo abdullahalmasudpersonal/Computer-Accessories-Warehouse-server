d882d777b80ec588551dce59c2cc7ff20576e4b8
masud <abdullahalmasud01737906772@gmail.com>
2022-09-15 19:36:33 +0600

    small another update

--- index.js
+++ index.js
@@ -15,21 +15,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iagltqc.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-/*  function verifJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: 'UnAuthorized access' });
-  }
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-    if (err) {
-      return res.status(403).send({ message: 'Forbidden access' })
-    }
-    req.decoded = decoded;
-    next();
-  });
-} */
 function verifJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
@@ -77,20 +62,6 @@ async function run() {
     });
 
     // order collection api
-    /*   app.get('/order', async (req, res) => {
-        const decodedEmail = req.decoded.email;
-        const email = req.query.email;
-        if (email === decodedEmail) {
-          const query = { email: email };
-          const cursor = orderCollection.find(query);
-          const orders = await cursor.toArray();
-          res.send(orders);
-        }
-        else{
-          res.status(403).send({message:'forbidden access'})
-        }
-      })  */
-
     app.get('/order', verifJWT, async (req, res) => {
       const decodedEmail = req.decoded.email;
       const email = req.query.email;
@@ -104,13 +75,6 @@ async function run() {
         res.status(403).send({ message: 'forbidden access' })
       }
     });
-
-    /*  app.get('/order', async (req, res) => {
-       const query = {};
-       const cursor = orderCollection.find(query);
-       const orders = await cursor.toArray();
-       res.send(orders);
-     }); */
 
     app.post('/order', async (req, res) => {
       const order = req.body;
